d05d2f9f0301b5680268045793da5de0d89e7225
Gabriel Correia <140673462+GabrielGabuu@users.noreply.github.com>
2026-02-20 19:09:36 -0300

    a

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,25 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    public void Play()
-    {
-        SceneManager.LoadScene("SceneTwo");
-=======
 
     public string gameSceneName;
     public void Play()
     {
         SceneManager.LoadScene(gameSceneName);
->>>>>>> Stashed changes
     }
 
     public void Quit()
     {
         Application.Quit();
     }
-<<<<<<< Updated upstream
 }
-=======
-}
->>>>>>> Stashed changes
